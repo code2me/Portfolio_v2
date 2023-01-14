504ab7615450f10cf34f400b53a571d16ec7a3be
Navneet Bahuguna <navneetbahuguna007@gmail.com>
2023-01-14 16:09:25 +0530

    added nav section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
 import './nav.css'
+import { AiOutlineHome } from "react-icons/ai";
+import { BiUserCircle, BiMessageSquareDetail } from "react-icons/bi";
+import {BsCodeSlash} from 'react-icons/bs'
+import {MdWorkOutline} from 'react-icons/md'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <a
+        href='#'
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <BiUserCircle />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <MdWorkOutline />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <BsCodeSlash />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 }
 
 export default Nav
