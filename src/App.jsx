a0c2fc939b78e3b6cd85e327c146500db213d8d1
Navneet Bahuguna <navneetbahuguna007@gmail.com>
2023-01-18 01:11:59 +0530

    vanta and three to update background and EmailJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -6,10 +5,52 @@ import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import { useEffect, useRef, useState } from "react";
+import FOG from 'vanta/dist/vanta.fog.min';
+import * as THREE from "three";
+
 
 const App = () => {
+
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const vantaRef = useRef(null)
+
+  useEffect(() => {
+    const threeScript = document.createElement("script");
+    threeScript.setAttribute("id", "threeScript");
+    threeScript.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+    );
+    document.getElementsByTagName("head")[0].appendChild(threeScript);
+
+    if (!vantaEffect) {
+      setVantaEffect(
+        FOG({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: true,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          highlightColor: 0x4db5ff,
+          midtoneColor: 0x1f1f38,
+          lowlightColor: 0x4db5ff,
+          baseColor: 0x000,
+        })
+      );
+    }
+    return () => {
+      if(threeScript) {
+        threeScript.remove();
+      }
+      if (vantaEffect) vantaEffect.destroy();
+    }
+  }, [vantaEffect])
+
   return (
-    <>
+    <div className='vanta' ref={vantaRef}>
         <Header />
         <Nav />
         <About />
@@ -17,7 +58,7 @@ const App = () => {
         <Portfolio />
         <Contact />
         <Footer />
-    </>
+    </div>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,28 +7,49 @@ import { BsTwitter } from "react-icons/bs";
 const Footer = () => {
   return (
     <footer>
-      <a href="#about" className="footer__logo"> Navneet Bahuguna </a>
-      
+      <a href="#about" className="footer__logo">
+        Navneet Bahuguna
+      </a>
+
       <ul className="permalinks">
-        <li> <a href="#about">Home</a> </li>
-        <li> <a href="#about">About</a> </li>
-        <li> <a href="#experience">experience</a> </li>
-        <li> <a href="#portfolio">portfolio</a> </li>
-        <li> <a href="#contact">contact</a> </li>
+        <li>
+          <a href="#about">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">contact</a>
+        </li>
       </ul>
 
       <div className="footer__socials">
-        <a href={Data.LINKEDIN}> <SiLinkedin /> </a>
-        <a href={Data.TWITTER}> <BsTwitter /> </a>
-        <a href={Data.LEETCODE}> <SiLeetcode /> </a>
-        <a href={Data.INSTA}> <SiInstagram /> </a>
+        <a href={Data.LINKEDIN}>
+          <SiLinkedin />
+        </a>
+        <a href={Data.TWITTER}>
+          <BsTwitter />
+        </a>
+        <a href={Data.LEETCODE}>
+          <SiLeetcode />
+        </a>
+        <a href={Data.INSTA}>
+          <SiInstagram />
+        </a>
       </div>
 
       <div className="footer__copyright">
-        <small>&copy; Navneet Bahuguna. All rights reserved</small>
+        <small>&copy; All rights reserved</small> <br />
+        <small>Created by Navneet Bahuguna</small>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
